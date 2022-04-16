ba7ad6f4850b0170a9d2dd2f9f71c7a4b5c3502c
Elturan <elturanfcb@gmail.com>
2022-04-16 17:11:54 +0400

    drag and drop ready

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const list = document.querySelector('.list')
 const btn = document.querySelector('button')
 const plus = document.querySelector('.plus')
 const add = document.querySelector('.button')
+const drag = document.querySelector('.drag')
+inputdiv.style.display = 'block'
 
 
 plus.addEventListener('mouseover', () => {
@@ -18,29 +20,65 @@ btn.addEventListener('mouseout', () => {
     add.style.display = 'none'
     btn.style.backgroundColor = 'white'
 })
+let id=1
 btn.addEventListener('click', () => {
-    let li = document.createElement('li')
-    let img1 = document.createElement('img')
-    img1.className = 'delete'
-    img1.src = '/img/delete.svg'
-    img1.alt = ''
-    let p = document.createElement('p')
-    p.innerHTML = input.value
-    li.prepend(p)
-    li.append(img1)
-    list.append(li)
-    input.value = ''
-    img1.addEventListener('mouseover', (e) => {
-        e.target.src = '/img/deletehover.svg'
-    })
-    img1.addEventListener('mouseout', (e) => {
-        e.target.src = '/img/delete.svg'
-    })
-    img1.addEventListener('click', (e) => {
-        e.target.parentElement.remove()
-    })
+    if (inputdiv.style.display == 'block') {
+        let li = document.createElement('li')
+        li.draggable = 'true'
+        li.id = id
+        id++
+        let img1 = document.createElement('img')
+        img1.className = 'delete'
+        img1.src = '/img/delete.svg'
+        img1.alt = ''
+        let p = document.createElement('p')
+        p.innerHTML = input.value
+        li.prepend(p)
+        li.append(img1)
+        drag.ondragover = allowDrop;
+        function allowDrop(event) {
+            event.preventDefault()
+        }
 
+        drag.addEventListener('dragover', (e) => {
+            e.preventDefault()
+        })
 
+        li.ondragstart = dragLi;
+        function dragLi(event) {
+            event.dataTransfer.setData('id', event.target.id)
+            drag.style.display = 'block'
+        }
+
+        drag.ondrop = drop;
+        function drop(event) {
+            let itemId = event.dataTransfer.getData('id');
+            if (itemId) {
+                event.target.append(document.getElementById(itemId))
+                let li2=document.querySelectorAll('.drag li')
+                li2.forEach(item=>{
+                    item.style.display='none'
+                })
+                drag.style.display='none'
+            }
+        }
+        list.append(li)
+        input.value = ''
+        img1.addEventListener('mouseover', (e) => {
+            e.target.src = '/img/deletehover.svg'
+        })
+        img1.addEventListener('mouseout', (e) => {
+            e.target.src = '/img/delete.svg'
+        })
+        img1.addEventListener('click', (e) => {
+            e.target.parentElement.remove()
+        })
+    }
+    else if (inputdiv.style.display == 'none') {
+        inputdiv.style.display = 'block'
+        list.style.display = 'none'
+        input.focus()
+    }
 })
 
 const del = document.querySelector('.delete')
@@ -56,10 +94,10 @@ del.addEventListener('click', () => {
 })
 let click = 0
 filter_az.addEventListener('click', (e) => {
-    if (click==0) {
+    if (click == 0) {
         list.style.display = 'block'
         inputdiv.style.display = 'none'
-        click=1
+        click = 1
     }
     else if (click == 1) {
         e.target.style.display = 'none'
@@ -75,7 +113,7 @@ filter_az.addEventListener('click', (e) => {
         p.forEach((item, index) => {
             item.innerText = task[index]
         })
-        click=0
+        click = 0
     }
 })
 filter_az_hover.addEventListener('click', (e) => {
@@ -97,3 +135,4 @@ filter_za_hover.addEventListener('click', (e) => {
     list.style.display = 'none'
     inputdiv.style.display = 'block'
 })
+
